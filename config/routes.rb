4e7341f27f14a2aa7be827861f9a9dc991418186
Ahmadwali zadron <wzadron@msudenver.edu>
2019-11-13 16:14:54 -0700

    comment

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  resources :parts
+  resources :makes
+  resources :cars do
+    collection do
+      get 'search'
+    end
+  end
+  root to: "cars#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
